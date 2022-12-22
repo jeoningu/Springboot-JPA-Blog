9c317d0c2fe3024d53e25d820d9965ce8c96c7fc
jeoningu <doremipasolsol1@gmail.com>
2022-12-23 00:07:26 +0900

    refactor : handleArgumentException 반환타입에 ResponseDto 적용  - ResponseDto에 필드 2개 : 에러코드, 에러메세지

--- src/main/java/com/jig/blog/handler/GlobalExceptionHandler.java
+++ src/main/java/com/jig/blog/handler/GlobalExceptionHandler.java
@@ -1,15 +1,19 @@
 package com.jig.blog.handler;
 
+import com.jig.blog.dto.ResponseDto;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestController;
 
+// TODO : ResponseEntity 적용 필요할 듯? ajax fail에서 받는게 안 됨.
 @ControllerAdvice
 @RestController
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(value = Exception.class)
-    public String handleArgumentException(Exception e) {
-        return "<h1>" + e.getMessage() + "</h1>";
+    public ResponseDto<String> handleArgumentException(Exception e) {
+        // TODO : postman에서 테스트 했을 때 error 메세지 마지막 줄만 보여서 원인 파악 제대로 안 됨
+        return new ResponseDto<String>(HttpStatus.INTERNAL_SERVER_ERROR.value(), e.getMessage());
     }
 }
