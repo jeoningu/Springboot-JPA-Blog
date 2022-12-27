0d1590ff3f5159d3d0c7128bd63ed3456cdaa806
jeoningu <doremipasolsol1@gmail.com>
2022-12-28 00:58:16 +0900

    feat : ExceptionHandler 에서 error 발생 위치 콘솔에서 확인 할 수 있게 수정
     - debug 모드일 때는 전체 error 출력
      * 사유1. JAVA의 Reflection을 이용하여 예외를 추적하는 것이라 많은 오버헤드가 발생할 수 있다.
      * 사유2. 메서드 스택정보를 취합하기 때문에 서버 부하의 원인이기도 하다.
      * 사유3. 내부적으로 System.err을 사용하는데 비용이 비싼편이다.

--- src/main/java/com/jig/blog/handler/GlobalExceptionHandler.java
+++ src/main/java/com/jig/blog/handler/GlobalExceptionHandler.java
@@ -1,19 +1,25 @@
 package com.jig.blog.handler;
 
 import com.jig.blog.dto.ResponseDto;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.logging.LoggingSystem;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestController;
 
+
 // TODO : ResponseEntity 적용 필요할 듯? ajax fail에서 받는게 안 됨.
 @ControllerAdvice
 @RestController
 public class GlobalExceptionHandler {
+    private Logger log = LoggerFactory.getLogger(getClass());
 
     @ExceptionHandler(value = Exception.class)
     public ResponseDto<String> handleArgumentException(Exception e) {
-        // TODO : postman에서 테스트 했을 때 error 메세지 마지막 줄만 보여서 원인 파악 제대로 안 됨
-        return new ResponseDto<String>(HttpStatus.INTERNAL_SERVER_ERROR.value(), e.getMessage());
+        log.error("{}",e.getStackTrace());
+        log.debug("{}",e);
+        return new ResponseDto<String>(HttpStatus.INTERNAL_SERVER_ERROR.value(), e.toString());
     }
 }
